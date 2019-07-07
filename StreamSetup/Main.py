79fe6f2ee473e2e346ca8c758829891687bc4b1e
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-07-07 13:14:36 -0700

    added logging

--- StreamSetup/Main.py
+++ StreamSetup/Main.py
@@ -1,3 +1,10 @@
-import webbrowser
-webbrowser.open("https://www.youtube.com/livestreaming/")
+import logging
+from logging import debug
 
+if __name__=="__main__":
+	logging.basicConfig(level=logging.DEBUG,
+		format= '%(asctime)s - %(levelname)s - %(message)s')
+
+import webbrowser
+debug("Opening Youtube Livestreaming")
+webbrowser.open("https://www.youtube.com/livestreaming/")
