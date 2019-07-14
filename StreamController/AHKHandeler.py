2018b3aede1d34b8178d0390114dd002bf34375e
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-07-14 13:11:30 -0700

    converted all indentation to tabs, and new function to bring windows to front

--- StreamController/AHKHandeler.py
+++ StreamController/AHKHandeler.py
@@ -1,5 +1,6 @@
 from ahk import AHK
 from ahk.window import Window
+from enum import Enum
 
 import logging
 from logging import debug
@@ -12,136 +13,148 @@ import subprocess
 from threading import Thread
 import time
 import webbrowser
+
+class WindowClassEnum(Enum):
+	CHROME = 1    
+
 class AHKHandeler():
 
-    def __init__(self, stream_name:str, obs_start_hotkey="^+J", obs_stop_hotkey="^!J"):
-        """
-            Stream name
-        """
-        self.ahk = AHK()
-        self.get_OBS()
-        self.get_ProPresenter()
-        self.obs_start_stream_hotkey = obs_start_hotkey
-        self.obs_stop_stream_hotkey = obs_stop_hotkey
-        debug(self.obs_start_stream_hotkey)
+	WINDOW_CLASSES = WindowClassEnum
 
-        time.sleep(1)
+	def __init__(self, stream_name:str, obs_start_hotkey="^+J", obs_stop_hotkey="^!J"):
+		"""
+			Stream name
+		"""
+		self.ahk = AHK()
+		self.get_OBS()
+		self.get_ProPresenter()
+		self.obs_start_stream_hotkey = obs_start_hotkey
+		self.obs_stop_stream_hotkey = obs_stop_hotkey
+		debug(self.obs_start_stream_hotkey)
 
-        self.stream_title = stream_name
+		time.sleep(1)
 
-    def get_ProPresenter(self):
-        self.ProPresenter = self.ahk.win_get("ProPresenter - Registered To: VALLEY CHRISTAIN CENTER")
-        return self.ProPresenter
+		self.stream_title = stream_name
 
-    def propresenter_send(self, key, window=None):
-        old_window = self.ahk.active_window
-        if self.ahk.active_window == self.get_ProPresenter():
-            self.ahk.send(key)
-        if window == None:
-            window = self.get_ProPresenter()
-            logging.warning(f"sending {key} to ProPresenter")
-            window.activate()
-            time.sleep(2)
-            self.ahk.send(key)
-            time.sleep(.5)
-            old_window.activate()
+	def get_ProPresenter(self):
+		self.ProPresenter = self.ahk.win_get("ProPresenter - Registered To:" +
+		" VALLEY CHRISTAIN CENTER")
+		return self.ProPresenter
 
-    def bring_to_front(self, window_class=None, preset_classes="Chrome"):
-        if window_class == None:
-            if preset_classes == "Chrome":
-                window_class = "Chrome_WidgetWin_1"
-        self.ahk.run_script(f"WinActivate, ahk_class {window_class}")
-        return
+	def propresenter_send(self, key, window=None):
+		old_window = self.ahk.active_window
+		if self.ahk.active_window == self.get_ProPresenter():
+			self.ahk.send(key)
+		if window == None:
+			window = self.get_ProPresenter()
+			logging.warning(f"sending {key} to ProPresenter")
+			window.activate()
+			time.sleep(2)
+			self.ahk.send(key)
+			time.sleep(.5)
+			old_window.activate()
 
-    def get_OBS(self):
-        self.OBS = self.ahk.win_get("OBS")
-        if self.OBS.id == "":
-            self.open_OBS()
-            self.OBS = self.ahk.win_get("OBS")
-        return self.OBS
+	def bring_to_front(self, preset_classes, window_class=None):
+		if window_class == None:
+			if preset_classes == self.WINDOW_CLASSES.CHROME:
+				window_class = "Chrome_WidgetWin_1"
+		self.ahk.run_script(f"WinActivate, ahk_class {window_class}")
+		return
 
-    def open_OBS(self):
-       self.ahk.run_script("CoordMode, Mouse, Screen\n"+
-       "MouseMove, 516, 998 \n Click")
-       time.sleep(1)
-       return
+	def get_OBS(self):
+		self.OBS = self.ahk.win_get("OBS")
+		if self.OBS.id == "":
+			self.open_OBS()
+			self.OBS = self.ahk.win_get("OBS")
+		return self.OBS
 
-    def OBS_send(self, key, window=None):
-        old_window = self.ahk.active_window
-        if window == None:
-            window = self.get_OBS()
-        logging.warning(f"sending {key} to obs")
-        window.activate()
-        time.sleep(.5)
-        self.ahk.send(key)
-        time.sleep(.5)
-        old_window.activate()
+	def open_OBS(self):
+		self.ahk.run_script("CoordMode, Mouse, Screen\n"+
+			"MouseMove, 516, 998 \n Click")
+		time.sleep(1)
+		return
+ 
+	def OBS_send(self, key, window=None):
+		old_window = self.ahk.active_window
+		if window == None:
+			window = self.get_OBS()
+		logging.warning(f"sending {key} to obs")
+		window.activate()
+		time.sleep(.5)
+		self.ahk.send(key)
+		time.sleep(.5)
+		old_window.activate()
 
-    def chrome_facebook_live_start(self):
-        webbrowser.open("https://www.facebook.com/CenterEvents1/")
+	def chrome_facebook_live_start(self):
+		webbrowser.open("https://www.facebook.com/CenterEvents1/")
 
-        # Upstairs deployment settings
-        thread = Thread(target = self.start_stream_facebook, args=(8000, (430, 593),
-        (719, 152), self.stream_title, (1018, 518), (1174, 922)))
+		# Upstairs deployment settings
+		thread = Thread(target = self.setup_stream_facebook, args=(8000, (430, 593),
+		(719, 152), self.stream_title, (1018, 518)))
 
-        # Nick's Laptop
-        # thread = Thread(target=self.start_stream_facebook, args=(8000, (711, 741), 
-        # (1084, 190), "A really cool title", (1577, 649), (1804, 960)))
-        thread.start()
+		# Nick's Laptop
+		# thread = Thread(target=self.setup_stream_facebook, args=(8000, (711, 741), 
+		# (1084, 190), "A really cool title", (1577, 649), (1804, 960)))
+		thread.start()
 
-    def start_stream_facebook(
-        self,
-        # Facebook related
-        wait_time:int, live_position:tuple, connect_position:tuple, stream_title:str,
-        stream_label_position:tuple, stream_go_live_position:tuple,
-        obs_window = None
-        ):
+	def setup_stream_facebook(
+		self,
+		# Facebook related
+		wait_time:int, live_position:tuple, connect_position:tuple, stream_title:str,
+		stream_label_position:tuple,
+		obs_window = None
+		):
 
-        if obs_window == None:
-            obs_window = self.get_OBS()
-        if stream_title == None:
-            raise ValueError("No stream title was given")
+		if obs_window == None:
+			obs_window = self.get_OBS()
+		if stream_title == None:
+			raise ValueError("No stream title was given")
 
-        self.ahk.run_script(f"#NoEnv \n Sleep {wait_time} \n "+
-            # Click Live
-            f"MouseMove, {live_position[0]}, {live_position[1]} \n"+
-            "Sleep 250 \n Click \n Sleep 5000 \n"+
-            # Click connect
-            f"MouseMove, {connect_position[0]}, {connect_position[1]} \n Sleep 250 \n"+
-            "Click \n Sleep 1500 \n"
-                )
+		self.ahk.run_script(f"#NoEnv \n Sleep {wait_time} \n "+
+			# Click Live
+			f"MouseMove, {live_position[0]}, {live_position[1]} \n"+
+			"Sleep 250 \n Click \n Sleep 5000 \n"+
+			# Click connect
+			f"MouseMove, {connect_position[0]}, {connect_position[1]} \n Sleep 250 \n"+
+			"Click \n Sleep 1500 \n"
+				)
 
-        self.end_stream_position = stream_go_live_position
+		# Start the OBS stream
+		self.OBS_send(self.obs_start_stream_hotkey)
+		time.sleep(2)
 
-        # Start the OBS stream
-        self.OBS_send(self.obs_start_stream_hotkey)
-        time.sleep(2)
+		# Click go live button on facebook
+		self.ahk.run_script(f"#NoEnv \n"+
+			# Click Title label
+			f"MouseMove, {stream_label_position[0]}, {stream_label_position[1]},"+
+			f"\n Sleep 250 \n Click \n Send {stream_title} \n"
+			)
 
-        # Click go live button on facebook
-        self.ahk.run_script(f"#NoEnv \n"+
-            # Click Title label
-            f"MouseMove, {stream_label_position[0]}, {stream_label_position[1]},"+
-            f"\n Sleep 250 \n Click \n Send {stream_title} \n"+
-            f"MouseMove, {stream_go_live_position[0]}, {stream_go_live_position[1]}\n"#+
-            "Sleep 3000 \n Click"
-            )
+		return
 
-        return
+	def start_facebook_stream(self, stream_go_live_position=(1174, 922)):
+		original_window = self.ahk.active_window
+		self.bring_to_front(self.WINDOW_CLASSES.CHROME)
+		time.sleep(1)
+		self.ahk.run_script(f"MouseMove, {stream_go_live_position[0]},"+
+		f" {stream_go_live_position[1]}\n"+
+			"Sleep 250 \n Click", blocking = False)
+		original_window.activate()
 
-    def stop_facebook_stream(self, end_stream_position:tuple):
+	def stop_facebook_stream(self, end_stream_position=(1174, 922)):
 
-        #self.OBS_send(self.obs_stop_stream_hotkey)
-        self.bring_to_front()
-        
-        self.ahk.run_script(f"MouseMove, {self.end_stream_position[0]},"+
-            f" {self.end_stream_position[1]}"+
-            "\n Sleep 250 \n Click")
-        self.OBS_send("^!j")
+		self.OBS_send(self.obs_stop_stream_hotkey)
+		self.bring_to_front(self.WINDOW_CLASSES.CHROME)
+		
+		self.ahk.run_script(f"MouseMove, {self.end_stream_position[0]},"+
+			f" {self.end_stream_position[1]}"+
+			"\n Sleep 250 \n Click", blocking = False)
+		self.OBS_send("^!j")
 
-        raise ReferenceError
+		raise ReferenceError
 
 if __name__ == "__main__":
-    ahker = AHKHandeler()
-    ahker.chrome_facebook_live_start()
-    time.sleep(30)
-    ahker.stop_facebook_stream((1804, 960))
+	ahker = AHKHandeler()
+	ahker.chrome_facebook_live_start()
+	time.sleep(30)
+	ahker.stop_facebook_stream((1804, 960))
