a0916fa48844dba6807451a4c5fd7a5eb1648742
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-07-27 23:56:28 -0700

    handeler for files

--- StreamController/option_loader.py
+++ StreamController/option_loader.py
@@ -1,19 +1,17 @@
 import atexit
 import pathlib2
+import json
 import os
-import shelve
 
-path = pathlib2.Path(os.path.join(os.path.abspath(__file__)))
-path = path.parents[0]
-path = path/"options"/"data"
-
-class ShelveHandeler:
+class OptHandle:
     def __init__(self):
-        self.shelf = shelve.open(str(path))
-        atexit.register(close)
+        path = pathlib2.Path(os.path.abspath(__file__)).parents[1]/"options.json"
+        with open(str(path)) as file:
+            self.dict = json.loads(file.read())
+        atexit.register(self.dump_and_close)
 
-    def close(self):
-        self.shelf.close()
+    def dump_and_close(self):
+        json.dumps(self.dict)
 
-    def assign(self, key, value):
-        self.shelf[key] = value        
+op = OptHandle()
+print(op.dict)
