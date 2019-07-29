8486250abc109e7b6831701a98b355bb63f221b2
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-07-28 18:30:45 -0700

    import logging

--- StreamController/Main.py
+++ StreamController/Main.py
@@ -1,4 +1,10 @@
 import wx
 import Gui
 
+import logging
+from logging import debug
+if __name__=="__main__":
+	logging.basicConfig(level=logging.DEBUG,
+		format= '%(asctime)s - %(levelname)s - %(message)s')
+
 controllerGui = Gui.ChurchGui()
