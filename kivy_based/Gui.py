49c5cd3b34f76640116a47fcc6a919d96c13fb89
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-08-05 21:44:57 -0600

    more work

--- kivy_based/Gui.py
+++ kivy_based/Gui.py
@@ -1,12 +1,32 @@
+import kivy
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty
 
 Window.size = (400, 400)
+
+class Controller(ScreenManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class StartupScreen(Screen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class StartupController(AnchorLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def on_submit(self)
+
+class MainScreen(Screen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 class StreamController(AnchorLayout):
     def __init__(self, **kwargs):
@@ -19,9 +39,6 @@ class SceneController(AnchorLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-class Controller(FloatLayout):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
 class GuiApp(App):
     def build(self):
