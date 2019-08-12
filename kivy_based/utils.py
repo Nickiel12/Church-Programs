544a7ffaea6e45ee80dc4d579dd790f878d77f66
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-08-12 11:19:18 -0700

    added a threaded tkinter popup window

--- kivy_based/utils.py
+++ kivy_based/utils.py
@@ -1,39 +1,73 @@
 import atexit
 import json
+import math
 import threading
 import time
-import wx
+from tkinter import Tk, Label, StringVar
+from tkinter.font import Font
+import tkinter
 import pathlib2
 import os
 
+class ChangeableText:
+    def __init__(self, unchanging_text, default_additional):
+        self.text = unchanging_text
+        self.value = default_additional
+
+    def set_value(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f"{self.text} {self.value}"
+
 class WarningPopup:  
+    stop = False
+    
+    def __init__(self, *args, **kwargs):    
+        self.win_x_size = 400
+        self.win_y_size = 150
+        self.top_label_text = "Do Not Touch The Computer"
+        self.top_2_label_text = "This window will close\n when the program is finished"
+        self.current_label_text = "No current Process"    
+        self.time_till_label_text = ChangeableText("Time will next process:", "0.0")
+        
     def open(self):
-        self.app = wx.App()
-        self.create()
+        self.thread = threading.Thread(target=self.create)
+        self.thread.start()
             
-    def create(self):
-        self.popup = PopupFrame()
-        self.popup.Show()
-        self.app.MainLoop()
+    def create(self, run_wait_time = .1):
+        self.root = Tk()
+        pos_right = int(self.root.winfo_screenwidth()/2 -self.win_x_size/2)
+        pos_up = int(self.root.winfo_screenheight()/3 - self.win_y_size/2) 
+        self.root.geometry(f"{self.win_x_size}x{self.win_y_size}+{pos_right}+{pos_up}")
+        
+        self.top_font = Font(self.root, "Arno 15", name="Arno")
+        top_var = StringVar()
+        top_label = Label(self.root, textvariable=top_var, font="Arno").pack()
+        top_2_var = StringVar()
+        top_2_label = Label(self.root, textvariable=top_2_var).pack()
+        current_var = StringVar() 
+        current_label = Label(self.root, textvariable=current_var).pack()
+        time_var = StringVar()
+        time_till_label = Label(self.root, textvariable=time_var).pack()
 
+        while self.stop == False:
+            top_var.set(self.top_label_text)
+            top_2_var.set(self.top_2_label_text)
+            current_var.set(self.current_label_text)
+            time_var.set(self.time_till_label_text)
+            try:
+                self.root.update()
+            except tkinter._tkinter.TclError:
+                return
+            time.sleep(run_wait_time)
+        self.root.quit()
+            
     def close(self):
-        self.popup.Close()
-
-class PopupFrame(wx.Frame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(parent = None, title="Start Stream")
-        self.build_panel()
-
-        self.SetSize(0, 0, 300, 250)
-        self.Center()
-
-    def build_panel(self):
-        panel = wx.Panel()
-        sizer = wx.BoxSizer()
-        panel.SetSizer(sizer)
-
-        warning_label = wx.StaticText(self, label="Do not touch the computer")
-        sizer.Add(warning_label, flags=wx.SizerFlags().Center())
+        print("closing thread")
+        self.stop = True
+        print("thread closed")
+        return
 
 def Settings():
     path = pathlib2.Path(os.path.abspath(__file__)).parent/"options.json"
@@ -58,5 +92,11 @@ class DotDict(dict):
 if __name__ == "__main__":
     popup = WarningPopup()
     popup.open()
-    time.sleep(5)
+    time_to_wait = 10
+    current_count = 0
+    for i in range(time_to_wait):
+        for i in range(10):
+            current_count += .1
+            popup.time_till_label_text.set_value(round(time_to_wait - current_count, 1))
+            time.sleep(.1)
     popup.close()
