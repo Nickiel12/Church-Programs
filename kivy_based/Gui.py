16a98219fe5244b9bb2ccaa99ed1351c7d03adee
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-08-16 14:30:15 -0400

    moved logic code out of classes

--- kivy_based/Gui.py
+++ kivy_based/Gui.py
@@ -12,10 +12,10 @@ from tkinter import messagebox
 import time
 import threading
 
-from exceptions import PopupError, PopupNotExist, PopupClosed
+from exceptions import PopupError, PopupNotExist, PopupClosed, PrematureExit
 from dialogs import Question, WarningPopup
 if True == False:
-    from kivy_based.exceptions import PopupError, PopupNotExist, PopupClosed
+    from kivy_based.exceptions import PopupError, PopupNotExist, PopupClosed, PrematureExit
     from kivy_based.utils import WarningPopup
     from kivy_based.dialogs import Question, WarningPopup
 
@@ -26,6 +26,67 @@ if __name__=="__main__":
         format= '%(asctime)s - %(levelname)s - %(message)s')
 
 Window.size = (400, 400)
+
+def on_startup_button_submit(stream_name, startup_controller):
+    print(f"stream_name is: {stream_name}")
+    thread = threading.Thread(target=_run_startup, args=(stream_name,))
+    thread.start()
+
+def sleep_check(time_to_sleep, event_to_check:threading.Event):
+    last_time = 0
+    end_time = time.time() + time_to_sleep
+    while True:
+        try:
+            if event_to_check.is_set():
+                raise PrematureExit("Timer caught event set")
+            time_left = end_time - time.time()
+            #debug(f"time_left is {time_left}")
+            #debug(f"end_time is {end_time}")
+            if time_left != last_time:
+                last_time = time_left
+                if time_left <= 0:
+                    break
+            time.sleep(.1)
+        except KeyboardInterrupt:
+            raise PrematureExit("Keyboard Inturrupt caught in sleep_check")
+
+def redo_startup():
+    App.get_running_app().root.current = "StartupScreen"
+
+def _run_startup(stream_name, *args):
+    try:
+        popup = WarningPopup()
+        popup.open()
+        
+        popup.set_task("Task One", 20)
+        sleep_check(20, popup.timer_event)
+        popup.set_task("Task Two", 10)
+        sleep_check(10, popup.timer_event)
+    except KeyboardInterrupt:
+        if popup.timer_thread and popup.timer_thread.isAlive():
+            popup.timer_event.set()
+    except (PopupNotExist, PrematureExit):
+        debug("Popup was closed unexpectedly")
+        if Question("Setup was canceled before it was finished\n"+
+            "Would you like to restart the program?", "Python"):
+            try:
+                popup.close()
+            except PopupClosed:
+                pass
+            redo_startup()
+        else:
+            print("the user said no to the question")
+        print("done with the question")
+    finally:
+        try:
+            popup.close()
+        except PopupClosed:
+            if Question("Setup was canceled before it was finished\n"+
+                "Would you like to restart the program?", "Python"):
+                debug("yes")
+                redo_startup()
+            else:
+                debug("no")    
 
 class Controller(ScreenManager):
     def __init__(self, *args, **kwargs):
@@ -38,50 +99,9 @@ class StartupScreen(Screen):
 class StartupController(AnchorLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
-    def on_submit(self, stream_name, *args):
-        debug(f"stream_name is: {stream_name}")
-        debug(f"on_submit args are: {args}")
-        self.complete_flag = threading.Event()
-        thread = threading.Thread(target=self._run_startup, args=(stream_name,))
-        thread.start()
 
-    def _run_startup(self, stream_name, *args):
-        try:
-            popup = WarningPopup()
-            popup.open()
-            
-            popup.set_task("Task One", 20)
-            time.sleep(20)
-            popup.set_task("Task Two", 10)
-            time.sleep(10)
-        except KeyboardInterrupt:
-            if popup.timer_thread and popup.timer_thread.isAlive():
-                popup.timer_event.set()
-        except PopupNotExist:
-            debug("Popup was closed unexpectedly")
-            if Question("Setup was canceled before it was finished\n"+
-                "Would you like to restart the program?", "Python"):
-                debug("yes")
-                try:
-                    popup.close()
-                except PopupClosed:
-                    pass
-                self._run_startup(stream_name)
-            else:
-                debug("no")
-            debug("done with the question")
-        finally:
-            try:
-                popup.close()
-                self.complete_flag.set()
-            except PopupClosed:
-                if Question("Setup was canceled before it was finished\n"+
-                    "Would you like to restart the program?", "Python"):
-                    debug("yes")
-                    self._run_startup(stream_name)
-                else:
-                    debug("no")
+    def on_submit(self, stream_name, *args):
+        on_startup_button_submit(stream_name, self)
 class MainScreen(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
