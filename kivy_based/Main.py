cfcab9b53f735ddfaceecdfe47e14882f274967f
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-08-19 10:54:31 -0700

    opens obs when started

--- kivy_based/Main.py
+++ kivy_based/Main.py
@@ -1,3 +1,13 @@
+import pathlib2
+import os
+
 from Gui import GuiApp
+
+def open_obs():
+    path = pathlib2.Path(
+        "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"+
+        "\\OBS Studio\\OBS Studio (64bit).lnk")
+    os.startfile(str(path))
+
 app = GuiApp()
 app.run()
