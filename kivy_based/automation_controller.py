94ea0d645202aac39e996638fb6cae50302e0ced
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-08-29 15:35:00 -0700

    bug fix, propresenter next not called

--- kivy_based/automation_controller.py
+++ kivy_based/automation_controller.py
@@ -29,7 +29,7 @@ def with_popup(func):
 
 class AutomationController:
     def __init__(self, settings, default_browser="CHROME"):
-        self.exe_path = str(pathlib2.Path(os.path.abspath(__file__)).parent/"ahk_exe"/"window_opener.exe")
+        self.ahk_files_path = pathlib2.Path(os.path.abspath(__file__)).parent/"ahk_exe"
         self.app = App.get_running_app()
         self.sett = self.app.settings
         self.platform_settings = self.sett[f"setup_{self.sett.streaming_service}"]
@@ -37,11 +37,11 @@ class AutomationController:
     def give_window_focus(self, window_to_focus):
         if window_to_focus.lower() == "obs":
             Logger.debug("WindowController: Changing active window to OBS")
-            subprocess.call([self.exe_path, self.sett.windows.obs_re])
+            subprocess.call([str(self.ahk_files_path/"window_opener.exe"), self.sett.windows.obs_re])
             time.sleep(.1)
         elif window_to_focus.lower() == "chrome":
             Logger.debug("WindowController: Changing active window to Chrome")
-            subprocess.call([self.exe_path, self.sett.windows.chrome_re])
+            subprocess.call([str(self.ahk_files_path/"window_opener.exe"), self.sett.windows.chrome_re])
             time.sleep(.1)
 
     #@threaded
@@ -77,11 +77,11 @@ class AutomationController:
     @threaded
     def propre_send(self, hotkey):
         if hotkey.lower() == "next":
-            subprocess.call([self.exe_path, self.sett.general.clicker_forward])
+            subprocess.call([str(self.ahk_files_path/"propresenter_send.ahk"), self.sett.general.clicker_forward])
             Logger.debug(f"Sending to propresenter: {self.sett.hotkeys.general.clicker_forward}")
             time.sleep(.2)
         elif hotkey.lower() == "prev":
-            subprocess.call([self.exe_path, self.sett.general.clicker_backward])
+            subprocess.call([str(self.ahk_files_path/"propresenter_send.ahk"), self.sett.general.clicker_backward])
             Logger.debug(f"Sending to propresenter: {self.sett.hotkeys.general.clicker_backward}")
             time.sleep(.2)
 
