89863b28bff0f1781c7f0ec0c4de175b09cf162a
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2019-09-16 15:50:10 -0700

    added command generator for loading startup apps

--- kivy_based/Main.py
+++ kivy_based/Main.py
@@ -1,13 +1,22 @@
 import os
 import pathlib2
 from kivy.app import App
-from Gui import GuiApp
 
+from Gui import GuiApp
+from utils import Settings
 from webserver import start_web_server
-path = pathlib2.Path(
-                     "C:\\ProgramData\\Microsoft\\Windows\\Start Menu" +
-                     "\\Programs\\OBS Studio\\OBS Studio (64bit).lnk")
-os.startfile(str(path))
+
+settings = Settings()
+
+programs_to_open = {}
+for name, value in settings.startup:
+    if name[:3] == "open" and value == True:
+        program = name[4:]
+        programs_to_open[program] = settings.startup[str(program)+"_path"]
+
+for i in programs_to_open.keys():
+    os.startfile(programs_to_open[i])
+
 gui_app = GuiApp()
 
 if __name__ == '__main__':
