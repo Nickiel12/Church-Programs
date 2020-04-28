7f96636eb1a14b5c50f8f6980a91050dd3960467
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2020-04-28 13:40:15 -0700

    applied formatting

--- webserver_only/new_main.py
+++ webserver_only/new_main.py
@@ -7,15 +7,15 @@ from utils import DotDict
 
 import logging
 logging.basicConfig(level=logging.DEBUG,
-     format= '[%(asctime)s][%(levelname)s] %(message)s', datefmt='%H:%M:%S')
+                    format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%H:%M:%S')
 #datefmt='%m/%d/%Y %H:%M:%S'
 logger = logging.getLogger("Main")
+
 
 class MasterState:
 
     OPTIONS_FILE_PATH = pathlib.Path(".").parent/"extras"/"options.json"
     settings = None
-
 
     def __init__(self):
         self.update_settings()
@@ -27,16 +27,19 @@ class MasterState:
             with open(self.OPTIONS_FILE_PATH) as file:
                 json_file = json.load(file)
             dot_dict = DotDict(json_file)
-            path = self.OPTIONS_FILE_PATH.parent / "options" / str(dot_dict.streaming_service + ".json")
+            path = self.OPTIONS_FILE_PATH.parent / "options" / \
+                str(dot_dict.streaming_service + ".json")
             with open(path) as file:
                 json_file_2 = json.load(file)
-            dot_dict["setup_" + dot_dict.streaming_service] = DotDict(json_file_2)
+            dot_dict["setup_" +
+                     dot_dict.streaming_service] = DotDict(json_file_2)
             self.settings = dot_dict
             logger.info("Successfully updated settings from file")
         except (json.JSONDecodeError) as e:
             logger.warning("Loading settings from file failed")
             logger.error(e)
             self.settings = tmp_settings
+
 
 if __name__ == "__main__":
     MasterState()
