1863af0ee92403ee4db541ed3539b80123ef0e58
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-07-23 22:15:26 -0700

    added an Enum for sub_scenes

--- android_server_rewrite/Classes/StreamEvents.py
+++ android_server_rewrite/Classes/StreamEvents.py
@@ -3,6 +3,16 @@ from enum import auto
 
 
 class StreamEvents(enum.Flag):
+
+    class SubScenes(enum.IntEnum):
+        CAMERA_NONE = auto()
+        CAMERA_TOP_RIGHT = auto()
+        CAMERA_BOTTOM_RIGHT = auto()
+        CAMERA_BOTTOM_LEFT = auto()
+        SCREEN_NONE = auto()
+        SCREEN_TOP_RIGHT = auto()
+        SCREEN_BOTTOM_RIGHT = auto()
+
     CAMERA_SCENE = auto()
     SCREEN_SCENE = auto()
     SPECIAL_SCENE = auto()
