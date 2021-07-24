f83813f7a7fe9f23f1f24a34fdcc9e0230fb297d
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2021-07-23 22:49:07 -0700

    separated the camera and screen enums

--- android_server_rewrite/Classes/SubScenes.py
+++ android_server_rewrite/Classes/SubScenes.py
@@ -3,10 +3,14 @@ from enum import auto
 
 
 class SubScenes(enum.IntEnum):
-    CAMERA_NONE = auto()
-    CAMERA_TOP_RIGHT = auto()
-    CAMERA_BOTTOM_RIGHT = auto()
-    CAMERA_BOTTOM_LEFT = auto()
-    SCREEN_NONE = auto()
-    SCREEN_TOP_RIGHT = auto()
-    SCREEN_BOTTOM_RIGHT = auto()
+
+    class Camera(enum.IntEnum):
+        CAMERA_NONE = auto()
+        CAMERA_TOP_RIGHT = auto()
+        CAMERA_BOTTOM_RIGHT = auto()
+        CAMERA_BOTTOM_LEFT = auto()
+
+    class Screen(enum.IntEnum):
+        SCREEN_NONE = auto()
+        SCREEN_TOP_RIGHT = auto()
+        SCREEN_BOTTOM_RIGHT = auto()
